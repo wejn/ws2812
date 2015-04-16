e26c7f2a5ce2c191742b39095e9b96e2c5161a8e
Michal Jirku <box@wejn.org>
2015-04-16 17:45:55 +0200

    Make build process all better

--- extconf.rb
+++ extconf.rb
@@ -1,4 +1,23 @@
+#!/usr/bin/env ruby
+
 require 'mkmf'
 #$libs = append_library($libs, "lib/libws2812-RPi.a")
+Dir.chdir(File.join(File.dirname(__FILE__), 'lib')) do
+	system "make"
+	fail "building lib failed" unless $?.exitstatus.zero?
+end
+system *%w[swig2.0 -Wall -ruby ws2812.i]
+fail "swig failed" unless $?.exitstatus.zero?
 find_library('ws2812-RPi', 'init', 'lib')
+dummy_makefile 'fofoa'
 create_makefile 'ws2812'
+
+File.open('Makefile', 'at') do |mk|
+	mk.puts <<EOF
+clean: clean-generated
+
+clean-generated:
+	rm -f Makefile ws2812_wrap.c
+	make -Clib clean
+EOF
+end
